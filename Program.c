cf8ac9f8b0590fc7e25eb851e3390961c2c86d74
girishgoud <34244908+girishgoud@users.noreply.github.com>
2019-04-19 18:18:14 +0530

    merger completed

--- Program.c
+++ Program.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
+#include<iostream>
+using namespace std;
 struct job{
 	int prid;
 	int art;
 	int but;
 	int tat;
+	int cmpt;
 	int wat;
 	int rbt;
 }f[100], s[100], m[100];
@@ -11,92 +14,88 @@ struct job{
 int n, fc=0, sc=0, mc=0;
 int quanta;
 
-void RR(){
-	int time=0,i, cCount=0;
-	while(1){
-		for(i=0; i<mc; i++){
-			if(m[i].rbt>quanta){
-				time += quanta; m[i].rbt -= quanta;
-			}
-			else if(m[i].rbt ==0){ continue;
-			}
-			else{
-				time += m[i].rbt; m[i].rbt=0; cCount++;
-			}
-		}
-		if(time==120 || cCount == mc) break;
-	}
-
-}
+void RR()
+{
+	
 
 void merg(){
-	int min= fc, i, flag=1, r=0, maxr=0;
-	if( fc>0 && sc>0){
-		if(min>sc){ flag=2; min=sc;	}
-		while(r != min){
-			if(flag==1){
-				if(f[r].art == s[maxr].art){
-					m[mc]= f[r]; r++; mc++;
-					m[mc]= s[maxr]; mc++; maxr++;
-				}
-				else if(f[r].art < s[maxr].art){
-					m[mc]= f[r]; r++; mc++;
-				}
-				else{
-					m[mc]= s[maxr]; maxr++; mc++;
-				}		
+	int isc=0, ifc= 0, min, flag;
+	if( fc!=0  && sc!=0){
+		while(isc<sc && ifc<fc){
+			if(f[ifc].art == s[isc].art){
+				m[mc] = f[ifc];
+				mc++;
+				ifc++;
+				m[mc]= s[isc];
+				mc++;
+				isc++;
+	
 			}
-			else{
-				if(s[r].art== f[maxr].art){
-					m[mc]= f[maxr]; mc++; maxr++;
-					m[mc]= s[r]; mc++; r++;
+			else if(f[ifc].at < s[isc].at){
+				m[mc]= f[ifc];
+				mc++;
+				ifc++;
+			}
+			else if(f[ifc].at > s[isc].at){
+				m[mc]= s[isc];
+				mc++;
+				isc++;
+			}
+			else;
+			
+		}
+		if(mc != (fc+sc)){
+			if(fc!=ifc){
+				while(ifc!=fc){
+					m[mc]= f[ifc];
+					mc++;
+					ifc++;
 				}
-				else if(s[r].art < f[maxr].art){
-					m[mc]= s[r]; mc++; r++;
+				
+			}
+			else if(sc!=isc){
+				while(isc!=sc){
+					m[mc]= s[isc];
+					mc++;
+					isc++;
 				}
-				else{
-					m[mc] = f[maxr]; mc++; maxr++;
-				}
+				
 			
 			}
 		}
-		if(min!=sc){
-			while(maxr!=sc){
-				m[mc]= s[maxr]; mc++; maxr++;
-			}	
-		}
-		else{
-			while(maxr!=fc){
-				m[mc]= f[maxr]; mc++; maxr++;
-			}
-		}
-	}	
-	else if(sc==0){
-		while(r!=fc){
-			m[mc]= f[r]; mc++; r++;
-		}
+		
 	}
 	else if(fc==0){
-		while(r!=sc){
-			m[mc]= s[r]; mc++; r++;
+		while(isc!=sc){
+			m[mc]= s[isc];
+			mc++;
+			isc++;
+		}
+
+	}
+	else if(sc==0){
+		while(ifc!=fc){
+			m[mc]= f[ifc];
+			mc++;
+			ifc++;
 		}
 	}
 	else {
-		printf("\n Nothing to be done"); exit(0);
+		cout<<"NOT VALID" <<endl;
 	}
 }
 void printer(){
 	int i=0; 
 	for(i; i<mc; i++){
-		printf("\n\nId: %d Arrivaltime: %d", m[i].prid, m[i].art);
-		printf("\n\nId: %d Arrivaltime: %d, RBT: %d", m[i].prid, m[i].art, m[i].rbt);
+		
+		printf("\n\n\n Id: %d Arrival time: %d, RBT: %d, CMPT: %d", m[i].prid, m[i].art, m[i].rbt, m[i].cmpt);
 	}
 }
 
 void input(){
 	int map,  i, t;
 	printf("Enter total no of queries: "); scanf("%d", &n);
-	if(n==0) { printf("\n No of queries\n"); exit(0);}
+	if(n==0) { printf("\n No queries\n"); }
 	else{
 		printf("\nEnter Quanta for particular Process: "); scanf("%d", &quanta);
 		printf("\nEnter 1 for faculty $$$$$ 2 for student\n");
@@ -118,7 +117,7 @@ void input(){
 		}
 	}
 }
-void main(){
+        main(){
 	input();
 	merg();
 	RR();
